8cbfa9f10a1a864703a6035682faa5100c9247d6
Michael <michael@iptables.sh>
2024-04-19 15:36:19 +0200

    1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ val javaVersion: Int = 17
 
 allprojects {
     group = "com.noxcrew.interfaces"
-    version = "1.0.1-SNAPSHOT"
+    version = "1.0.1"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = javaVersion.toString()
