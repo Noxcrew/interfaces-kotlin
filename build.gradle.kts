3007bfc2348976fb78e084d110258e1ec3e9bcec
Aeltumn <daniel@goossens.ch>
2024-04-23 22:40:41 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ val javaVersion: Int = 21
 
 allprojects {
     group = "com.noxcrew.interfaces"
-    version = "1.1.0-SNAPSHOT"
+    version = "1.1.1-SNAPSHOT"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = javaVersion.toString()
