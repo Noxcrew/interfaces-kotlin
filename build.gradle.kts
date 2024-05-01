a1ae51ba068314b31ce0bcf83b48bac382230d80
Aeltumn <daniel@goossens.ch>
2024-05-01 11:49:58 +0200

    Make snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ val javaVersion: Int = 21
 
 allprojects {
     group = "com.noxcrew.interfaces"
-    version = "1.1.2"
+    version = "1.1.2-SNAPSHOT"
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = javaVersion.toString()
