85a9eddd88ee16f8f6bb5425e09e193e661f3172
Kieran Wallbanks <kieran.wallbanks@gmail.com>
2024-08-14 16:52:45 +0100

    feature: More grid position generator API
    - Makes GridPositionGenerator implement Iterable
    - Adds a utility constructor to create a GridBoxGenerator from 4 ints

--- interfaces/src/main/kotlin/com/noxcrew/interfaces/grid/GridBoxGenerator.kt
+++ interfaces/src/main/kotlin/com/noxcrew/interfaces/grid/GridBoxGenerator.kt
@@ -9,6 +9,11 @@ public data class GridBoxGenerator(
     private val max: GridPoint
 ) : GridPositionGenerator {
 
+    public constructor(x1: Int, y1: Int, x2: Int, y2: Int) : this(GridPoint(x1, y1), GridPoint(x2, y2))
+
+    override fun iterator(): Iterator<GridPoint> =
+        (min..max).iterator()
+
     override fun generate(): List<GridPoint> =
         (min..max).toList()
 }
--- interfaces/src/main/kotlin/com/noxcrew/interfaces/grid/GridPositionGenerator.kt
+++ interfaces/src/main/kotlin/com/noxcrew/interfaces/grid/GridPositionGenerator.kt
@@ -1,8 +1,12 @@
 package com.noxcrew.interfaces.grid
 
 /** Generates a set of [GridPoint]'s. */
-public fun interface GridPositionGenerator {
+public fun interface GridPositionGenerator : Iterable<GridPoint> {
 
     /** Returns a list of [GridPoint]'s. */
     public fun generate(): List<GridPoint>
+
+    override fun iterator(): Iterator<GridPoint> {
+        return generate().iterator()
+    }
 }
