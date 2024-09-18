045ec59a63e419efce2a3c7a12810b16213d94f0
Kieran Wallbanks <kieran.wallbanks@gmail.com>
2024-09-18 14:15:44 +0100

    feature: Add ChainGridPositionGenerator

---
+++ interfaces/src/main/kotlin/com/noxcrew/interfaces/grid/ChainGridPositionGenerator.kt
@@ -0,0 +1,20 @@
+package com.noxcrew.interfaces.grid
+
+/** A grid position generator that chains together two other generators. */
+public data class ChainGridPositionGenerator(
+    /** The first generator. */
+    private val first: GridPositionGenerator,
+    /** The second generator. */
+    private val second: GridPositionGenerator,
+) : GridPositionGenerator {
+
+    public companion object {
+        /** Adds two grid position generators together. */
+        public operator fun GridPositionGenerator.plus(second: GridPositionGenerator): ChainGridPositionGenerator =
+            ChainGridPositionGenerator(this, second)
+    }
+
+    override fun generate(): List<GridPoint> {
+        return first.generate() + second.generate()
+    }
+}
